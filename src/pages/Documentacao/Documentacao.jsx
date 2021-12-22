5807434b637a478e873b7e6a4ecc2fe557cb4d4f
Michelle Miranda <biamichelle@Michelles-MacBook-Pro.local>
2021-12-22 17:44:37 -0300

    [Biamichelle] Troca do link do livro

--- src/pages/Documentacao/Documentacao.jsx
+++ src/pages/Documentacao/Documentacao.jsx
@@ -54,15 +54,15 @@ const YoutubeEmbed = ({ embedId }) => (
                 <h2 className="cards-artigos">Artigos:</h2>
                     <ul className="lista">
                         <li className="linha"> MIRANDA, M. B.; WISNIEWSKI, A. ; MENEZES, A. ; PRIKLADNICKI, R. . <b>Teaching Software Engineering Remotely to Teenagers in Social Vulnerability Conditions during the Coronavirus Pandemic: Challenges and Lessons Learned.</b> In: XXIV - CIbSE - Iberoamerican Conference on Software Engineering, 2021, San Jose, Costa Rica. XXIV - CIbSE - Iberoamerican Conference on Software Engineering, 2021.</li>
-                        <li className="linha">Caio Steglich, Anielle Lisboa, Rafael Prikladnicki, Sabrina Marczak, Michael da Costa Móra, Alejandro Olchik, Nelice Heck, Yasser Rachid, and Guilherme Ghidorsi. 2020 <b>Agile Accelerator Program: From Industry-Academia Collaboration to Effective Agile Training</b>. In Proceedings of the 34th Brazilian Symposium on Software Engineering (SBES '20). Association for Computing Machinery, New York, NY, USA, 21–30. 
-                            <Link href="https://www.amazon.com.br/Aceleradora-%C3%A1gil-inclusiva-conhecimento-tecnol%C3%B3gico-ebook/dp/B08G386YC7" target="_blank">DOI:https://doi.org/10.1145/3422392.3422485</Link></li>
+                        <li className="linha">Caio Steglich, Anielle Lisboa, Rafael Prikladnicki, Sabrina Marczak, Michael da Costa Móra, Alejandro Olchik, Nelice Heck, Yasser Rachid, and Guilherme Ghidorsi. 2020. <b>Agile Accelerator Program: From Industry-Academia Collaboration to Effective Agile Training</b>. In Proceedings of the 34th Brazilian Symposium on Software Engineering (SBES '20). Association for Computing Machinery, New York, NY, USA, 21–30. 
+                            <Link href="https://dl.acm.org/doi/10.1145/3422392.3422485" target="_blank">DOI:https://doi.org/10.1145/3422392.3422485</Link></li>
                     </ul>   
             </div>
             <div className="cards-livros">
                     <img src={ImageLivro} className="livro" alt="imagem do livro" />
                     <div className="bloco-livro">
                         <h3 className="h3-documentacao">Livro:</h3>
-                        <p className="paragrafo-documentacao">Aceleradora Ágil e Inclusiva, unindo conhecimento, projetos e pessoas para construir um futuro tecnológico justo. <Link href="https://www.pucrs.br/tecnopuc/wp-content/uploads/sites/110/2019/12/LivroThoughtWorksFinalMontado.pdf" target="_blank">Download</Link></p>
+                        <p className="paragrafo-documentacao">Aceleradora Ágil e Inclusiva, unindo conhecimento, projetos e pessoas para construir um futuro tecnológico justo. <Link href="https://www.amazon.com.br/Aceleradora-%C3%A1gil-inclusiva-conhecimento-tecnol%C3%B3gico-ebook/dp/B08G386YC7" target="_blank">Download</Link></p>
                     </div>
             </div> 
             <h2 className='cards-videos'>Vídeos:</h2>
