d112251ebf694bc2ba60714cea174f8528a61391
Michelle Miranda <biamichelle@Michelles-MacBook-Pro.local>
2022-04-26 10:33:29 -0300

    Arrumando foto da turma 07

--- src/pages/Galeria/photos.js
+++ src/pages/Galeria/photos.js
@@ -20,11 +20,6 @@ export const photos = [
       height: 3
     },
     {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-      width: 3,
-      height: 4
-    },
-    {
       src: "assets/t05/t5.jpeg",
       width: 4,
       height: 3
@@ -36,7 +31,7 @@ export const photos = [
     },
     {
       src: "assets/t07/t7.jpeg",
-      width: 4,
+      width: 7,
       height: 3
     },
     {
@@ -45,6 +40,10 @@ export const photos = [
       height: 3
     }
   ];
+
+  // src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+  //     width: 3,
+  //     height: 4
 
   // "https://source.unsplash.com/I1ASdgphUH4/800x599"
 
