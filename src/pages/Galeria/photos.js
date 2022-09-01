f97d30fc3d3bf6ea9370a6f3938793fdfbe5c71f
Michelle Miranda <biamichelle@Michelles-MacBook-Pro.local>
2022-09-01 12:34:51 -0300

    inserindo foto da turma 09 - segundo semestre de 2022

--- src/pages/Galeria/photos.js
+++ src/pages/Galeria/photos.js
@@ -1,52 +1,56 @@
-export const photos = [
-    {
-      src: "assets/t01/t1.jpg",
-      width: 4,
-      height: 3
+export const photos = [{
+        src: "assets/t01/t1.jpg",
+        width: 4,
+        height: 3
     },
     {
-      src: "assets/t02/t2.png",
-      width: 4,
-      height: 3
+        src: "assets/t02/t2.png",
+        width: 4,
+        height: 3
     },
     {
-      src: "assets/t03/t3.png",
-      width: 4,
-      height: 3
+        src: "assets/t03/t3.png",
+        width: 4,
+        height: 3
     },
     {
-      src: "assets/t04/t4.jpg",
-      width: 4,
-      height: 3
+        src: "assets/t04/t4.jpg",
+        width: 4,
+        height: 3
     },
     {
-      src: "assets/t05/t5.jpeg",
-      width: 4,
-      height: 3
+        src: "assets/t05/t5.jpeg",
+        width: 4,
+        height: 3
     },
     {
-      src: "assets/t06/t6.png",
-      width: 4,
-      height: 3
+        src: "assets/t06/t6.png",
+        width: 4,
+        height: 3
     },
     {
-      src: "assets/t07/t7.jpeg",
-      width: 7,
-      height: 3
+        src: "assets/t07/t7.jpeg",
+        width: 7,
+        height: 3
     },
     {
-      src: "assets/t08/t8.jpeg",
-      width: 4,
-      height: 3
+        src: "assets/t08/t8.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "assets/t09/t9.jpeg",
+        width: 4,
+        height: 3
     }
-  ];
+];
 
-  // src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-  //     width: 3,
-  //     height: 4
+// src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+//     width: 3,
+//     height: 4
 
-  // "https://source.unsplash.com/I1ASdgphUH4/800x599"
+// "https://source.unsplash.com/I1ASdgphUH4/800x599"
 
-  // src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-  // width: 4,
-  // height: 3
+// src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+// width: 4,
+// height: 3
