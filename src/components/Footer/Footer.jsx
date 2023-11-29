307032dc286e77d992c5e864bb81849348b7c376
inclusiva <inclusiva.aceleradora@gmail.com>
2023-11-29 14:29:03 -0300

    data

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.styless.css'
 const Footer = () => {
   return(
     <div className="footer">
-      <span>Feito com <BsFillSuitHeartFill size={16} color="#fff"/> por Biamichelle, Eduarda (t07) e Willian (t07) - 2021 e Bruna Gonzaga 2023</span>
+      <span>Feito com <BsFillSuitHeartFill size={16} color="#fff"/> por Biamichelle, Eduarda (t07) e Willian (t07) - 2021 e Bruna Gonzaga - 2023</span>
     </div>
   )
 }
